429a5485a295ebd7fe22ea3f44ef1a46211e4c90
Allan19Prieto <allanbanilla@gmail.com>
2024-09-10 18:47:29 -0600

    Ejemplos Prolog

---
+++ Programación_Lógica/miembro.pl
@@ -0,0 +1,14 @@
+% Función Miembro %
+miembro(X,[X|_]).
+miembro(X,[_|R]):-miembro(X,R).
+
+% Función inversa %
+inversa(L1,[L]):-inversa(L1,[],L).
+inversa([],L,L).
+inversa([X|L1],L2,L3):-inversa(L1,[X|L2],L3).
+
+%  Cree las clausulas para longitud.  %
+%   Longitud([a,b,c,d,e],5).
+%   True
+
+
