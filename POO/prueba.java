abcf11326897e4f3b72a5a4509f66dd65bc16137
Allan19Prieto <allanbanilla@gmail.com>
2024-11-08 23:21:04 -0600

    Nuevas Carpetas

---
+++ POO/prueba.java
@@ -0,0 +1,8 @@
+package POO;
+// clase para correr un hora mundo
+public class prueba {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase HoraMundo
+        System.err.println("Creando un objeto de la clase HoraMundo");
+    }
+}
