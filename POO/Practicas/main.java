1cefc6166eba7dbd591913d20c7083ef7177cdd1
Allan19Prieto <allanbanilla@gmail.com>
2024-11-11 22:10:00 -0600

    Pruebas

--- POO/Practicas/main.java
+++ POO/Practicas/main.java
@@ -1,5 +1,14 @@
 package POO.Practicas;
 
 public class main {
+    public static void main(String[] args) {
+        vehiculo vehiculo = new vehiculo();
+        cuatroXcuatro cuatroXcuatro = new cuatroXcuatro();
+        modo modo = new modo();
+
+        vehiculo.combustible();
+        cuatroXcuatro.combustible();
+        modo.combustible();
+    }
     
 }
